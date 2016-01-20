7ef6a71f0c9b9f67685c6ddd7eceef6535385be4
Georgy Marrero <georgymarrero@gmail.com>
2016-01-20 14:13:32 -0800

    Add course action types

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -3,8 +3,15 @@
 var keyMirror = require('react/lib/keyMirror');
 
 module.exports = keyMirror({
+
   INITIALIZE: null,
+
   CREATE_AUTHOR: null,
   UPDATE_AUTHOR: null,
-  DELETE_AUTHOR: null
+  DELETE_AUTHOR: null,
+
+  CREATE_COURSE: null,
+  UPDATE_COURSE: null,
+  DELETE_COURSE: null
+
 });
