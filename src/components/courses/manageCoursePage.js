5f5c49f39556bda6a27278839bc34c2c7b2f97a1
Georgy Marrero <georgymarrero@gmail.com>
2016-01-20 14:18:40 -0800

    Add basic manage course page

---
+++ src/components/courses/manageCoursePage.js
@@ -0,0 +1,83 @@
+"use strict";
+
+var React = require('react');
+var Router = require('react-router');
+var CourseActions = require('../../actions/courseActions');
+var CourseStore = require('../../stores/courseStore');
+var toastr = require('toastr');
+
+var ManageCoursePage = React.createClass({
+  mixins: [
+    Router.Navigation
+  ],
+
+  statics: {
+    willTransitionFrom: function(transition, component) {
+      if (component.state.dirty && !confirm('Leave without saving?')) {
+        transition.abort();
+      }
+    }
+  },
+
+  getInitialState: function() {
+    return {
+      course: { id: '', title: '', length: '', author: '', category: '' },
+      errors: {},
+      dirty: false
+    };
+  },
+
+  componentWillMount: function() {
+    var courseId = this.props.params.id; // from the path '/course:id'
+
+    if (courseId) {
+      this.setState({course: CourseStore.getCourseById(courseId)});
+    }
+  },
+
+  setCourseState: function(event) {
+    this.setState({dirty: true});
+    var field = event.target.name;
+    var value = event.target.value;
+    this.state.course[field] = value;
+    return this.setState({course: this.state.course});
+  },
+
+  courseFormIsValid: function() {
+    var formIsValid = true;
+    this.state.errors = {}; // clear any previous errors.
+
+    this.setState({errors: this.state.errors});
+
+    return formIsValid;
+  },
+
+  getAuthorById: function(id) {
+    return AuthorStore.getAuthorById(id);
+  },
+
+  saveCourse: function(event) {
+    event.preventDefault();
+
+    if (!this.courseFormIsValid()) {
+      return;
+    }
+
+    if (this.state.course.id) {
+      CourseActions.updateCourse(this.state.course);
+    } else {
+      CourseActions.createCourse(this.state.course);
+    }
+
+    this.setState({dirty: false});
+    toastr.success('Course saved.');
+    this.transitionTo('courses');
+  },
+
+  render: function() {
+    return (
+    );
+  }
+});
+
+module.exports = ManageCoursePage;
